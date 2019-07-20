256ed2ba25ad7137622af9c1ae7936e8087f0bf6
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-19 23:22:45 -0700

    added java files

---
+++ src/main/java/UserException.java
@@ -0,0 +1,12 @@
+
+
+public class UserException extends Exception {
+	 public UserException() {
+	        super();
+	    }
+
+	    public UserException(String message) {
+	        super(message);
+	    }
+
+}
